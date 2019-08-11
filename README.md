ea04fb1e16f705ff2d9b0f5fc6c76faccff8fbc1
wangxd <wangxd@inke.cn>
2019-08-11 17:51:24 +0800

    geektime2pdf

---
+++ README.md
@@ -0,0 +1,53 @@
+# 极客时间专栏转换为PDF
+
+>说明：该项目仅仅只能用户个人学习使用，不能在商业中使用，若极客时间官方要求该代码仓库删除，请联系我进行删除
+
+## 使用方法
+
+### 配置信息
+
+在配置文件[config.js](./config,js)中修改配置所需要的信息
+
+```js
+/**
+ * 需要转换为 pdf 的配置信息 
+ */
+module.exports = {
+    url: 'https://time.geekbang.org/serv/v1/article', // 该配置项不需要改动
+    commentUrl: 'https://time.geekbang.org/serv/v1/comments', // 该配置项不需要改动
+    columnBaseUrl: 'https://time.geekbang.org/column/article/', // 该配置项不需要改动
+    columnName: '玩转VScode', // 专栏名称
+    firstArticalId: 18053, //专栏第一篇文章的ID
+    isdownloadVideo: false, // 是否下载音频
+    isComment: false, // 是否导出评论
+    cookie: 'cookie'
+};
+```
+
+* 上面的配置项**前三项是不需要修改**的, 只需要修改后面的专栏信息
+
+* 会自动生成一个`geektime_{{columnName}}` 的文件夹来保存导出的所有`pdf`文件, `columnName` 为上面配置的
+
+* `firstArticalId` 这个参数最好配置专栏第一篇文章的 `ID` ，这个可以获取专栏的所有的文章,若不是第一篇文章的`ID` 则获取的是该文章以及之后的文章
+
+* `cookie` 你在网页版登录后返回的`cookie`信息
+
+### 运行
+
+1. `git@github.com:greyireland/geektime_to_pdf.git` 在本地克隆下来
+2. 然后执行 `npm i` 安装依赖
+3. 编辑config.js配置
+4. 运行主程序 `node columnArticleList.js` 等待一段时间,生成 `PDF` 完成
+
+![](./image/geektime_run.png)
+
+## 导出结果
+
+![](./image/geektime_VScode_filelist.png)
+
+![](./image/geektime_file_content.png)
+
+## 合成为一个PDF
+```
+python merge.py /Users/hh/geek/geektime2pdf/algo combined.pdf
+```
---
+++ columnArticleList.js
@@ -0,0 +1,89 @@
+// 获取专栏文章列表
+const config = require('./config.js');
+const superagent = require('superagent');
+const utils = require('./utils');
+const path = require('path');
+const generaterPdf = require('./generaterPdf.js');
+const downloadAudio = require('./downloadAudio.js');
+const downloadComment = require('./downloadComment.js');
+
+/**
+ * 执行方法
+ */
+(async function getColumnArticleList (firstArticalId){
+    await utils.createDir('geektime_'+config.columnName);
+    console.log('专栏文章链接开始获取');
+    let columnArticleUrlList = [];
+    let articalId = firstArticalId;
+    async function getNextColumnArticleUrl (){
+        try {
+            let res = await superagent.post(config.url)
+            .set({
+                'Content-Type': 'application/json',
+                'Cookie': config.cookie,
+                'Referer': config.columnBaseUrl + articalId,
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
+            }).send({
+                'id': articalId,
+                'include_neighbors': true
+            });
+            if (res.body && res.body.error && res.body.error.code){
+                console.log('error msg', res.body.error.msg);
+                throw new Error(res.body.error.msg);
+            };
+            console.log(res.body.data.article_title);
+            let columnArticle = res.body.data;
+
+            let articleInfo = {
+                articleTitle: columnArticle.article_title, // 文章标题
+                articalUrl: config.columnBaseUrl + articalId, // 文章地址
+                articleContent: columnArticle.article_content, // 文章内容
+                articleCover: columnArticle.article_cover, // 文章背景图
+                authorName: columnArticle.author_name, // 文章作者
+                articleCtime: utils.formatDate(columnArticle.article_ctime), // 文章创建时间 unix 时间戳 单位为 s 
+                articleNeighbors: columnArticle.neighbors,  //  上下篇文章信息
+                audioDownloadUrl: columnArticle.audio_download_url,
+                audioTitle: columnArticle.audio_title
+            };
+            columnArticleUrlList.push(articleInfo);
+            articleInfo.commentsTotal = 0;
+            articleInfo.commentsArr = [];
+            // 是否导出评论
+            if (config.isComment) {
+                let {commentsTotal, commentsArr} = await downloadComment(
+                    config.columnBaseUrl + articalId,
+                    articalId);
+                articleInfo.commentsTotal = commentsTotal;
+                articleInfo.commentsArr = commentsArr;
+            };
+            // 替换文章名称的 / 线， 解决路径被分割的问题
+            let useArticleTtle = columnArticle.article_title.replace(/\//g, '-');
+            //生成PDF 
+            await generaterPdf(articleInfo,
+                useArticleTtle + '.pdf',
+                path.resolve(__dirname, 'geektime_' + config.columnName)
+            );
+            // 是否下载音频
+            if (config.isdownloadVideo && columnArticle.audio_download_url) {
+                await downloadAudio(
+                    columnArticle.audio_download_url,
+                    useArticleTtle + '.mp3',
+                    path.resolve(__dirname, 'geektime_' + config.columnName)
+                );
+            };
+            // 判断是否还有下一篇文章
+            let neighborRight = columnArticle.neighbors.right;
+            if (neighborRight && neighborRight.id){
+                articalId = neighborRight.id;
+                await utils.sleep(1.5);
+                await getNextColumnArticleUrl();
+            };
+        } catch(err){
+            console.log(`访问 地址 ${config.columnBaseUrl + articalId} err`, err.message);
+        };
+    };
+    await getNextColumnArticleUrl(firstArticalId);
+    console.log('专栏文章链接获取完成');
+    // utils.writeToFile(`geektime_${config.columnName}`, JSON.stringify(columnArticleUrlList,null,4));
+    return columnArticleUrlList;
+})(config.firstArticalId);
